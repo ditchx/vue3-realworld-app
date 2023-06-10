e0920cc0ceb260c29bab4872d93a4a98061c78e6
Rolan Batungbakal <understasis@gmail.com>
2023-06-10 17:35:03 +0800

    feat: create tag list component

---
+++ src/components/TagList.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { useTags } from '@/services/tags';
+
+const { tags, getTags } = useTags()
+
+getTags()
+
+</script>
+<template>
+    <div class="tag-list">
+        <a v-for="tag in tags" :key="tag" href="" class="tag-pill tag-default">{{ tag }}</a>
+    </div>
+</template>
