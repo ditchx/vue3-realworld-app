b3d31b2543913492093d1ea97ab2479fb1827373
Rolan Batungbakal <understasis@gmail.com>
2023-06-10 17:40:42 +0800

    feat:  extract tags list and article feed into their own components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-
+import TagList from '@/components/TagList.vue';
+import ArticleFeed from '@/components/ArticleFeed.vue';
 </script>
 
 <template>
-  <main>
+  <div class="home-page">
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+    </div>
+    <div class="container page">
+      <div class="row">
+        <ArticleFeed />
+        <div class="col-md-3">
+          <div class="sidebar">
+            <p>Popular Tags</p>
+            <TagList />
+          </div>
+        </div>
 
-  </main>
+      </div>
+    </div>
+  </div>
 </template>
