3e3ab9d15821b2cba98dbeac27f18f776bd9f3bc
Rolan Batungbakal <understasis@gmail.com>
2023-06-13 18:40:15 +0800

    fix: replace $reset login/logout methods

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,19 +1,24 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { useLocalStorage } from '@vueuse/core'
-import { emptyUser } from '@/services/users'
+import { emptyUser, type User } from '@/services/users'
 
 export const useAuthStore = defineStore('auth', () => {
     const user = useLocalStorage('pinia/auth/login', emptyUser())
-    const isLoggedIn = computed(() => user.value.token != "")
+    const isLoggedIn = computed(() => user.value && user.value.token != "")
 
-    function $reset() {
+    function login(u: User) {
+        user.value = u
+    }
+
+    function logout() {
         user.value = null
     }
 
     return {
         user,
         isLoggedIn,
-        $reset
+        login,
+        logout
     }
 })
