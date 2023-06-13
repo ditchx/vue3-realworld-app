14cd1fcf83ff6f4fc25ca8f75fb00666e0c2c1f4
Rolan Batungbakal <understasis@gmail.com>
2023-06-13 19:08:29 +0800

    feat: add login and register routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -10,13 +11,16 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/AuthView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/AuthView.vue')
     }
+
   ]
 })
 
