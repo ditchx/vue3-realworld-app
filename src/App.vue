dc58aa2e1e7fbd0e1338d3390490e61a34d27e37
Rolan Batungbakal <understasis@gmail.com>
2023-06-15 15:12:36 +0800

    fix: add router key to router view to reload component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
 import TheHeader from './components/TheHeader.vue'
 import TheFooter from './components/TheFooter.vue'
+
+const route = useRoute()
+
 </script>
 
 <template>
   <TheHeader />
-  <RouterView />
+  <RouterView :key="route.fullPath" />
   <TheFooter />
 </template>
