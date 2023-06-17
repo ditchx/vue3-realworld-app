31d595c7a01f49575308bd992585da479ec49686
Rolan Batungbakal <understasis@gmail.com>
2023-06-18 07:23:31 +0800

    feat: emit setTag event on tag select

--- src/components/TagList.vue
+++ src/components/TagList.vue
@@ -2,11 +2,18 @@
 import { useTags } from '@/services/tags'
 
 const { tags, getTags } = useTags()
-
+const emits = defineEmits<{
+  (e: 'tagSelected', selected: string): void
+}>()
 getTags()
+
+function setTag(selected: string) {
+  emits('tagSelected', selected)
+}
+
 </script>
 <template>
   <div class="tag-list">
-    <a v-for="tag in tags" :key="tag" href="" class="tag-pill tag-default">{{ tag }}</a>
+    <a @click.prevent="setTag(tag)" v-for="tag in tags" :key="tag" href="" class="tag-pill tag-default">{{ tag }}</a>
   </div>
 </template>
