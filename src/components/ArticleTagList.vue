c9b439adddebbfe1b6ae7ee276269233dfdba186
Rolan Batungbakal <understasis@gmail.com>
2023-11-10 21:57:33 +0800

    feat: add component for article tags

---
+++ src/components/ArticleTagList.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  tags: string[]
+}>()
+
+</script>
+<template>
+  <ul v-if="props.tags.length" class="tag-list">
+    <li v-for="tag in props.tags" class="tag-default tag-pill tag-outline">{{ tag }}</li>
+  </ul>
+</template>
