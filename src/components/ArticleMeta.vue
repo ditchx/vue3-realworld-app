a07bcba9b8b551e3756026d9c1d78d452222e085
Rolan Batungbakal <understasis@gmail.com>
2023-11-11 21:56:16 +0800

    feat: show edit and delete article buttons when article author is logged in

--- src/components/ArticleMeta.vue
+++ src/components/ArticleMeta.vue
@@ -3,7 +3,11 @@ import { injectArticle } from '@/services/articles';
 import FavoriteButton from './FavoriteButton.vue';
 import { useDateFormat } from '@vueuse/core';
 import FollowUser from './FollowUser.vue';
+import { useAuthStore } from '@/stores/auth';
 const { article } = injectArticle()
+
+const store = useAuthStore()
+const isArticleOwner = store.user.username === article.value.author.username
 
 
 </script>
@@ -20,6 +24,12 @@ const { article } = injectArticle()
         <i class="ion-heart"></i>
       &nbsp; {{ article.favorited ? 'Unfavorite' : 'Favorite' }} Article <span class="counter">({{ article.favoritesCount }})</span>
     </favorite-button>
-    
+    <button v-if="isArticleOwner" class="btn btn-sm btn-outline-secondary">
+      <i class="ion-edit"></i> Edit Article
+    </button>
+    &nbsp;
+    <button v-if="isArticleOwner" class="btn btn-sm btn-outline-danger">
+      <i class="ion-trash-a"></i> Delete Article
+    </button>
   </div>
 </template>
