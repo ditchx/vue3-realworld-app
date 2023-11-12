d8ffa5ece99e8196cf1924b0b417ad089102a5d2
Rolan Batungbakal <understasis@gmail.com>
2023-11-12 19:02:46 +0800

    feat: add create article functionality

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,5 +1,62 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+import { useArticle } from '@/services/articles'
+import router from '@/router'
+
+const title = ref('')
+const description = ref('')
+const body = ref('')
+const tag = ref('')
+const tagList = ref<Set<string>>(new Set<string>())
+
+const store = useAuthStore()
+
+const { article, isLoading, lastError, addArticle } = useArticle()
+
+const formDisabled = computed(() => isLoading.value)
+
+async function publishArticle(): Promise<void> {
+  await addArticle({
+    title: title.value,
+    description: description.value,
+    body: body.value,
+    tagList: Array.from(tagList.value)
+  }, store.user.token) 
   
+  if (!lastError.value.length && article.value.slug) {
+    router.push({name: 'article', params: { slug: article.value.slug }})
+  }
+}
+
+function checkTag(event: Event) {
+  const key: string | null  = (event instanceof InputEvent) ? 
+    (event as InputEvent).data : (event as KeyboardEvent).key
+ 
+  if (key === null) {
+    return
+  }
+  
+  if (key == ',' || key == ' ' || key == 'Enter') {
+    if (key !== 'Enter') {
+      tag.value = tag.value.slice(0, -1)
+    }
+
+    tagList.value.add(tag.value)
+    tag.value = ''
+    return
+  }
+
+  if (!/^[0-9a-zA-Z\-\_]*$/.test(tag.value)) {
+    tag.value = tag.value.slice(0, -1)
+  }
+}
+
+
+function removeTag(tag: string) {
+  tagList.value.delete(tag)
+}
+
 </script>
 <template>
   <div class="editor-page">
@@ -7,34 +64,38 @@
       <div class="row">
         <div class="col-md-10 offset-md-1 col-xs-12">
           <ul class="error-messages">
-            <li>That title is required</li>
+              <li v-for="erroMsg in lastError" :key="erroMsg">{{ erroMsg }}</li>
           </ul>
-          <form>
+          <form @submit.prevent="">
             <fieldset>
               <fieldset class="form-group">
                 <input
+                  :disabled="formDisabled"
+                  v-model="title"
                   type="text"
                   class="form-control form-control-lg"
                   placeholder="Article Title"
                 />
               </fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="What's this article about?" />
+                <input :disabled="formDisabled" v-model="description" type="text" class="form-control" placeholder="What's this article about?" />
               </fieldset>
               <fieldset class="form-group">
                 <textarea
+                  :disabled="formDisabled"
+                  v-model="body"
                   class="form-control"
                   rows="8"
                   placeholder="Write your article (in markdown)"
                 ></textarea>
               </fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="Enter tags" />
+                <input v-model="tag" @input="checkTag" @keyup.enter="checkTag" @paste.prevent="" :disabled="formDisabled" type="text" class="form-control" placeholder="Enter tags" />
                 <div class="tag-list">
-                  <span class="tag-default tag-pill"> <i class="ion-close-round"></i> tag </span>
+                  <span v-for="tag in tagList" :key="tag" class="tag-default tag-pill">{{ tag }} <i @click="removeTag(tag)" class="ion-close-round"></i></span>
                 </div>
               </fieldset>
-              <button class="btn btn-lg pull-xs-right btn-primary" type="button">
+              <button @click.prevent="publishArticle" class="btn btn-lg pull-xs-right btn-primary" type="button">
                 Publish Article
               </button>
             </fieldset>
